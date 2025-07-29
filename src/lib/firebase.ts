a6d4f4a74642e717c7d61203bb47a77dd9033d4d
Max Dylan <maxdyllaann@gmail.com>
2025-07-29 20:21:05 +0000

    Por que não tô conseguindo criar minha conta pra testar o aplicativo ?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,10 +10,11 @@ const firebaseConfig = {
   apiKey: "AIzaSyAuJ6Aue0SJSxRzQ4GaEv7p1iRlythkeL0",
   authDomain: "fiscalflow-h2lyd.firebaseapp.com",
   projectId: "fiscalflow-h2lyd",
-  storageBucket: "fiscalflow-h2lyd.firebasestorage.app",
+  storageBucket: "fiscalflow-h2lyd.appspot.com",
   messagingSenderId: "337778816271",
   appId: "1:337778816271:web:b4c448701c86a33c2c3a7a"
 };
+
 
 // Initialize Firebase
 let app;
